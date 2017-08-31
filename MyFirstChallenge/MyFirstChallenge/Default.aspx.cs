8bc2ecc5c830237ce888fa57768b3baacc8a6d59
Joaquin Rodriguez <gio17joaquin@gmail.com>
2017-08-30 22:28:41 -0700

    First commit

---
+++ MyFirstChallenge/MyFirstChallenge/Default.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace MyFirstChallenge
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            string ageAmount = ageTextBox.Text;
+            string moneyAmount = moneyTextBox.Text;
+
+            string result = "At " + ageAmount + " years old, I would have expected you to have more than " + moneyAmount + " in your pocket.";
+
+            resultLabel.Text = result;
+        }
+    }
+}
