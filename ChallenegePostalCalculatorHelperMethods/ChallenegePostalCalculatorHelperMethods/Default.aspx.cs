129c91024a2e2442743ed2595761ac4cc844722a
Joaquin Rodriguez <gio17joaquin@gmail.com>
2017-09-17 09:47:01 -0700

    First Commit

---
+++ ChallenegePostalCalculatorHelperMethods/ChallenegePostalCalculatorHelperMethods/Default.aspx.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ChallenegePostalCalculatorHelperMethods
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void lengthTextBox_TextChanged(object sender, EventArgs e)
+        {
+            performChanged();
+        }
+
+        
+
+        protected void widthTextBox_TextChanged(object sender, EventArgs e)
+        {
+            performChanged();
+        }
+
+        protected void heightTextBox_TextChanged(object sender, EventArgs e)
+        {
+            performChanged();
+        }
+
+        protected void groundRadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            performChanged();
+        }
+
+        protected void airRadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            performChanged();
+        }
+
+        protected void nextdayRadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            performChanged();
+        }
+
+        private void performChanged()
+        {
+            // data
+            if (!valuesExist()) return;
+
+            // volume
+            int volume = 0;
+            if (!tryGetVolume(out volume)) return;
+
+            // Multipliers
+            double postageMultiplier = getPostageMultiplier();
+
+            // Cost
+            double cost = volume * postageMultiplier;
+
+            //Result
+            resultLabel.Text = String.Format("Your parcel will cost {0:C} to ship.", cost);
+
+        }
+
+
+        private bool valuesExist()
+        {
+            if (!airRadioButton.Checked
+                 && !groundRadioButton.Checked
+                 && !nextdayRadioButton.Checked)
+                return false;
+
+            if (widthTextBox.Text.Trim().Length == 0
+               || heightTextBox.Text.Trim().Length == 0)
+                return false;
+
+            return true;
+        }
+
+        private bool tryGetVolume(out int volume)
+        {
+            volume = 0;
+            int width = 0;
+            int height = 0;
+            int length = 0;
+
+            if (!int.TryParse(widthTextBox.Text.Trim(), out width)) return false;
+            if (!int.TryParse(heightTextBox.Text.Trim(), out height)) return false;
+            if (!int.TryParse(lengthTextBox.Text.Trim(), out length)) length = 1;
+
+            volume = width * height * length;
+            return true;
+
+
+        }
+
+        private double getPostageMultiplier()
+        {
+            if (groundRadioButton.Checked) return .15;
+            else if (airRadioButton.Checked) return .25;
+            else if (nextdayRadioButton.Checked) return .45;
+            else return 0;
+        }
+
+
+    }
+}
---
+++ MegaChallengeCasino/MegaChallengeCasino/Default.aspx.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace MegaChallengeCasino
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        Random random = new Random();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                // Display reel
+                string[] reels = new string[] { spinReel(), spinReel(), spinReel() };
+                displayImages(reels);
+                ViewState.Add("PlayersMoney", 100);
+                displayPlayersMoney();
+
+            }
+        }
+
+
+        protected void pullButton_Click(object sender, EventArgs e)
+        {
+            // click= display random img
+            int bet = 0;
+            if (!int.TryParse(betTextBox.Text, out bet)) return;
+
+            int winnings = pullLever(bet);
+            displayResult(bet, winnings);
+            adjustPalyersMoney(bet, winnings);
+            displayPlayersMoney();  
+        }
+
+        private void adjustPalyersMoney(int bet, int winnings)
+        {
+            int playersMoney = int.Parse(ViewState["PlayersMoney"].ToString());
+            playersMoney -= bet;
+            playersMoney += winnings;
+            ViewState["PlayersMoney"] = playersMoney;
+        }
+
+        private int pullLever(int bet)
+        {
+            string[] reels = new string[] { spinReel(), spinReel(), spinReel() };
+            displayImages(reels);
+            int multiplier = evaluateSpin(reels);
+            return bet * multiplier;
+        }
+
+        private int evaluateSpin(string[] reels)
+        {
+            // one bar then return 0
+            if (isBar(reels)) return 0;
+            
+            // three 7's return 100
+            if (isJackpot(reels)) return 100;
+
+            // one or more cherries return 2, 3, 4
+            int multiplier = 0;
+            if (isWinner(reels, out multiplier)) return multiplier;
+
+            return 0;
+
+        }
+
+        private bool isWinner(string[] reels, out int multiplier)
+        {
+            multiplier = determineMultiplier(reels);
+            if (multiplier > 0) return true;
+            return false;
+        }
+
+        private int determineMultiplier(string[] reels)
+        {
+            int cherryCount = determineCherryCount(reels);
+            if (cherryCount == 1) return 2;
+            if (cherryCount == 2) return 3;
+            if (cherryCount == 3) return 4;
+            return 0;
+
+        }
+
+        private int determineCherryCount(string[] reels)
+        {
+            int cherryCount = 0;
+            if (reels[0] == "Cherry") cherryCount++;
+            if (reels[1] == "Cherry") cherryCount++;
+            if (reels[2] == "Cherry") cherryCount++;
+            return cherryCount;
+        }
+
+
+        private bool isJackpot(string[] reels)
+        {
+            if (reels[0] == "Seven" && reels[1] == "Seven" && reels[2] == "Seven")
+                return true;
+            else
+                return false;
+        }
+
+        private bool isBar(string[] reels)
+        {
+            if (reels[0] == "Bar" || reels[1] == "Bar" || reels[2] == "Bar")
+                return true;
+            else
+                return false;
+        }
+
+        private string spinReel()
+        {
+            string[] images = new string[] { "Bar", "Bell", "Cherry", "Clover", "Diamond", "HorseShoe", "Lemon", "Orange", "Plum", "Seven", "Strawberry", "Watermellon" };
+            return images[random.Next(12)];
+        }
+
+        private void displayImages(string[] reels)
+        {
+            Image1.ImageUrl = "/Img/" + reels[0] + ".png";
+            Image2.ImageUrl = "/Img/" + reels[1] + ".png";
+            Image3.ImageUrl = "/Img/" + reels[2] + ".png";
+
+        }
+
+        private void displayResult(int bet, int winnings)
+        {
+            if (winnings > 0)
+                resultLabel.Text = String.Format("You bet {0:C} and won {1:C}!", bet, winnings);
+            else
+                resultLabel.Text = String.Format("Sorry, you lost {0:C}. Better luck next time.", bet);
+        }
+
+        private void displayPlayersMoney()
+        {
+            moneyLabel.Text = String.Format("Player's Money: {0:C}", ViewState["PlayersMoney"]);
+        }
+
+    }
+}
